0f7d3357bcf89547f0b10b2303aa65a818c33d8e
higracehuang <117108383+higracehuang@users.noreply.github.com>
2023-08-28 15:11:42 -0500

    Initial Commit

---
+++ KeyStrokeDetection/KeyStrokeDetectionApp.swift
@@ -0,0 +1,17 @@
+//
+//  KeyStrokeDetectionApp.swift
+//  KeyStrokeDetection
+//
+//  Created by Le Huang on 8/28/23.
+//
+
+import SwiftUI
+
+@main
+struct KeyStrokeDetectionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
